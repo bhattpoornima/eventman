64901b3dff23d211ff8f18132f36882ac0d6dfec
Poornima Bhatt <bhattpayal030@gmail.com>
2024-11-29 17:19:56 +0530

    updated

--- server.js
+++ server.js
@@ -4,21 +4,29 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const eventRoutes = require('./routes/event');
 const authRoutes = require('./routes/auth');
-// Initialize dotenv to load environment variables
-dotenv.config();
+
+dotenv.config();  // Initialize dotenv to load environment variables
 
 const app = express();
 
+// Allowing CORS from a dynamic origin (from .env)
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',  // Fallback for local development
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
-
 
 // Route for user authentication (registration and login)
 app.use('/api/auth', authRoutes);
 
 // Route for event management (CRUD operations)
-app.use('/api/events', eventRoutes); // Apply authMiddleware to protect event routes in the routes only
+app.use('/api/events', eventRoutes); 
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
@@ -38,5 +46,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-
