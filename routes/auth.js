69b7206d4a57daca699b36cef2593f7c3c741c91
Poornima Bhatt <bhattpayal030@gmail.com>
2024-11-29 17:04:03 +0530

    added error message

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,7 @@ router.post('/register',
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({ message: 'Validation errors occurred', errors: errors.array() });
         }
 
         const { name, email, password } = req.body;
